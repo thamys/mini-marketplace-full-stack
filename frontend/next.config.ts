d352429ee95a52fe8cf0fedd44683619d68cbdfc
Thamyres Guedes <thamyres@hous3.digital>
2026-03-10 15:09:21 -0300

    fix(frontend): disable standalone mode to fix Render deployment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
-import path from 'path';
+
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'standalone',
-  outputFileTracingRoot: path.join(__dirname, '../../'),
 };
 
 export default nextConfig;
