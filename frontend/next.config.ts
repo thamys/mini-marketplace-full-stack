97a5df1da6d57baa821192a9b6d7d5680299a1bd
Thamyres Guedes <thamyres@hous3.digital>
2026-03-11 10:52:07 -0300

    feat(frontend): update middleware and next.config for products

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,6 +5,19 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
   outputFileTracingRoot: path.join(__dirname, '../../'),
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
 };
 
 export default nextConfig;
